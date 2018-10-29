8381dd50742fcaaed04a3f5941b921f84000b8e9
Yuta Nakamura (smith) <nakamurayuta0204@gmail.com>
2018-10-29 18:40:46 +0900

    implement crop image and load_tensor

--- src/MITDataLoader.py
+++ src/MITDataLoader.py
@@ -1,21 +1,41 @@
 import os
 import numpy as np
 import pandas as pd
+from MITData import MITData
 
 class MITDataLoader:
-    def __init__(self, train=True):
+    def __init__(self, root_dir="../data/MIT_data", train=True):
         # self.train_index = read_csv(csffile)
         # self.test_index = read_csv(csffile)
         self.train = train
+        self.root_dir = root_dir
+
         if self.train:
-            self.root_dir = "../data/MIT_exp/training"
+            self.data_dir = os.path.join(root_dir, "training")
+        else:
+            self.data_dir = os.path.join(root_dir, "test")
+
+        self.index = pd.read_csv(os.path.join(self.root_dir, "train_index.csv"),
+                                    index_col="index")
+        self._i = 0
 
     # iter data set based on self.index
     def __iter__(self):
         return self
 
     def __next__(self):
-        pass
+        if self._i == self.index.shape[0]:
+            raise StopIteration
+        index = self._i
+        data = self.index.loc[self._i]
+        mitdata = MITData(data["directory"], data["filename"], data["train"])
+        self._i  += 1
+        return index, mitdata
+
+    def __getitem__(self, x):
+        data = self.index.loc[x]
+        mitdata = MITData(data["directory"], data["filename"], data["train"])
+        return mitdata
 
     # create csv file based on directory
     def parse_directory(self):
@@ -23,18 +43,18 @@ class MITDataLoader:
         # for data in directory
         # index.append({"directory": directory, "train":train, "filename": video,  "category": category})
         n = 0
-        for category in os.listdir(self.root_dir):
-            for movie in os.listdir(os.path.join(self.root_dir, category)):
+        for category in os.listdir(self.data_dir):
+            for movie in os.listdir(os.path.join(self.data_dir, category)):
                 index.append({"index": n, 
-                            "directory": os.path.join(self.root_dir, category), 
+                            "directory": os.path.join(self.data_dir, category), 
                             "filename":movie, "train":"train", "category":category})
                 n  += 1;
 
         df = pd.DataFrame(index)
         df = df.set_index("index")
-        df.to_csv(os.path.join("train_index.csv"), index_label="index")
+        df.to_csv(os.path.join(self.root_dir, "train_index.csv"), index_label="index")
 
 if __name__ == '__main__':
     train_loader = MITDataLoader(train=True)
-    train_loader.parse_directory()
-
+    data = train_loader[247]
+    data.crop_image()
